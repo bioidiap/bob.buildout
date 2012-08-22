1de752590c28cfc010a8ad2137ad25954cd23723
Andre Anjos <andre.dos.anjos@gmail.com>
2012-08-22 14:05:12 +0200

    Reset to version 0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bob.buildout.recipes',
-    version='0.1.1',
+    version='0.1.0',
     description="zc.buildout recipes to perform a variety of tasks required by Bob satellite packages",
     keywords=['buildout', 'sphinx', 'nose', 'recipe', 'eggs'],
     url='http://github.com/bioidiap/bob.buildout.recipes',
